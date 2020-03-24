737dce844349f575ac1ebde33c8a9450cb8b542b
Kevin Malakoff <kmalakoff@gmail.com>
2020-03-24 05:48:29 -0700

    Refactor for multiple exports

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-function deepReduce(array, fn, options) {
-  var value;
+var flattenDeep = require('./lib/flattenDeep');
+var joinDeep = require('./lib/joinDeep');
+var reduceDeep = require('./lib/reduceDeep');
 
-  for (var i = 0; i < array.length; i++) {
-    value = array[i];
-    if (Array.isArray(value)) options.memo = deepReduce(value, fn, options);
-    else options.memo = fn(options.memo, value, array, i);
-  }
-
-  return options.memo;
-}
-
-module.exports = function (array, fn, memo) {
-  var options = { memo };
-  deepReduce(array, fn, options);
-  return options.memo;
-};
+module.exports = reduceDeep;
+module.exports.flattenDeep = flattenDeep;
+module.exports.joinDeep = joinDeep;
+module.exports.reduceDeep = reduceDeep;
--- test/flattenDeep.test.js
+++ test/flattenDeep.test.js
@@ -1,34 +1,20 @@
-var assert = require("assert");
-var deepReduce = require("..");
+var assert = require('assert');
+var flattenDeep = require('..').flattenDeep;
 
-var flattenDeep = function (array) {
-  var length = array == null ? 0 : array.length;
-  return length
-    ? deepReduce(
-        array,
-        function (memo, value) {
-          memo.push(value);
-          return memo;
-        },
-        []
-      )
-    : [];
-};
-
-describe("flatten methods", function () {
+describe('flatten methods', function () {
   var array = [1, [2, [3, [4]], 5]];
 
-  it("should treat sparse arrays as dense", function () {
-    var array = [[1, 2, 3], Array(3)],
-      expected = [1, 2, 3];
+  it('should treat sparse arrays as dense', function () {
+    var array = [[1, 2, 3], Array(3)];
+    var expected = [1, 2, 3];
 
     expected.push(undefined, undefined, undefined);
     var actual = flattenDeep(array);
     assert.deepStrictEqual(actual, expected);
-    assert.ok("4" in actual);
+    assert.ok('4' in actual);
   });
 
-  // it.only("should work with extremely large arrays", function () {
+  // it("should work with extremely large arrays", function () {
   //   var expected = Array(5e5);
   //   try {
   //     assert.deepStrictEqual(flattenDeep([expected]), expected);
@@ -37,19 +23,19 @@ describe("flatten methods", function () {
   //   }
   // });
 
-  it("should work with empty arrays", function () {
+  it('should work with empty arrays', function () {
     var array = [[], [[]], [[], [[[]]]]];
 
     assert.deepStrictEqual(flattenDeep(array), []);
   });
 
-  it("should support flattening of nested arrays", function () {
+  it('should support flattening of nested arrays', function () {
     assert.deepStrictEqual(flattenDeep(array), [1, 2, 3, 4, 5]);
   });
 
-  it("should return an empty array for non array-like objects", function () {
-    var expected = [],
-      nonArray = { "0": "a" };
+  it('should return an empty array for non array-like objects', function () {
+    var expected = [];
+    var nonArray = { '0': 'a' };
 
     assert.deepStrictEqual(flattenDeep(nonArray), expected);
   });
