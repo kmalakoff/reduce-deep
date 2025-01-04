7cfd2ead60308ebfd43317c0f27c9b8486b13209
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:19:47 -0800

    Migrate to Biome and ts-dev-stack

--- test/spec/flattenDeep.test.js
+++ test/spec/flattenDeep.test.js
@@ -1,15 +1,15 @@
-var assert = require('assert');
-var constant = require('lodash.constant');
+const assert = require('assert');
+const constant = require('lodash.constant');
 
-var reduceDeep = require('../..');
+const reduceDeep = require('reduce-deep');
 
 function flattenDeep(array) {
-  var length = array == null ? 0 : array.length;
+  const length = array == null ? 0 : array.length;
 
   return length
     ? reduceDeep(
         array,
-        function (memo, value) {
+        (memo, value) => {
           memo.push(value);
           return memo;
         },
@@ -26,38 +26,37 @@ function flattenDeep(array) {
  * @returns {Object} Returns the converted `arguments` object.
  */
 function toArgs(array) {
-  return function () {
-    return arguments;
-  }.apply(undefined, array);
+  // biome-ignore lint/style/noArguments: <explanation>
+  return (() => arguments).apply(undefined, array);
 }
 
-var args = toArgs([1, 2, 3]);
+const args = toArgs([1, 2, 3]);
 
-describe('flatten methods', function () {
-  it('should flatten `arguments` objects', function () {
-    var array = [args, [args]];
+describe('flatten methods', () => {
+  it('should flatten `arguments` objects', () => {
+    const array = [args, [args]];
 
     assert.deepEqual(flattenDeep(array), [1, 2, 3, 1, 2, 3]);
   });
 
-  it('should treat sparse arrays as dense', function () {
-    var array = [[1, 2, 3], Array(3)];
-    var expected = [1, 2, 3];
+  it('should treat sparse arrays as dense', () => {
+    const array = [[1, 2, 3], Array(3)];
+    const expected = [1, 2, 3];
 
     expected.push(undefined, undefined, undefined);
-    var actual = flattenDeep(array);
+    const actual = flattenDeep(array);
     assert.deepEqual(actual, expected);
     assert.ok('4' in actual);
   });
 
-  it('should flatten objects with a truthy `Symbol.isConcatSpreadable` value', function () {
+  it('should flatten objects with a truthy `Symbol.isConcatSpreadable` value', () => {
     if (typeof Symbol === 'undefined' || !Symbol.isConcatSpreadable) return;
-    var object = { 0: 'a', length: 1 };
-    var array = [object];
-    var expected = constant(['a'])();
+    const object = { 0: 'a', length: 1 };
+    const array = [object];
+    const expected = constant(['a'])();
 
     object[Symbol.isConcatSpreadable] = true;
-    var actual = flattenDeep(array);
+    const actual = flattenDeep(array);
     assert.deepEqual(actual, expected);
   });
 
@@ -71,20 +70,20 @@ describe('flatten methods', function () {
   //   }
   // });
 
-  it('should work with empty arrays', function () {
-    var array = [[], [[]], [[], [[[]]]]];
+  it('should work with empty arrays', () => {
+    const array = [[], [[]], [[], [[[]]]]];
 
     assert.deepEqual(flattenDeep(array), []);
   });
 
-  it('should support flattening of nested arrays', function () {
-    var array = [1, [2, [3, [4]], 5]];
+  it('should support flattening of nested arrays', () => {
+    const array = [1, [2, [3, [4]], 5]];
 
     assert.deepEqual(flattenDeep(array), [1, 2, 3, 4, 5]);
   });
 
-  it('should return an empty array for non array-like objects', function () {
-    var nonArray = { 0: 'a' };
+  it('should return an empty array for non array-like objects', () => {
+    const nonArray = { 0: 'a' };
 
     assert.deepEqual(flattenDeep(nonArray), []);
   });
