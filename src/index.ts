20a36485699f23f17ef63932c94ed3045b086c26
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-02 00:02:13 -0700

    Upgrade isflattenable and add NestedArray type

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import isFlattenable from 'isflattenable';
 
+export type NestedArray<T> = T | NestedArray<T>[];
 export type ReduceFn<T, V> = (memo: V, item: T, array?: T[], index?: number) => V;
 
-export default function reduceDeep<T, V>(array: T[], fn: ReduceFn<T, V>, memo: V): V {
+export default function reduceDeep<T, V>(array: NestedArray<T>[], fn: ReduceFn<T, V>, memo: V): V {
   let result: V = memo;
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < (array as T[]).length; i++) {
     const value = array[i];
     if (isFlattenable(value)) result = reduceDeep<T, V>(value as T[], fn, result);
-    else result = fn(result, value, array, i);
+    else result = fn(result, value as T, array as T[], i);
   }
   return result;
 }
